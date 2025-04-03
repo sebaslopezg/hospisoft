95be7c8774205085c40f5fb8bd9a34d07d427fc3
sebas <sebaslg96@gmail.com>
2025-04-03 11:45:50 -0500

    cambios en clase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
 
