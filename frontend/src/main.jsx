fe00cd2ae5c7d4c4483718cfa910dd0dad71000e
lulo <lopez.gallego2611@gmail.com>
2025-04-07 07:35:57 -0500

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,6 @@
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <App />,
 )
-=======
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(<App />)
->>>>>>> 95be7c8774205085c40f5fb8bd9a34d07d427fc3
