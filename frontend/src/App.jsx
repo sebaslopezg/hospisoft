a6369b4546b45edf551de21cd64381f7cf394d6a
lulo <lopez.gallego2611@gmail.com>
2025-04-07 07:34:51 -0500

    added component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import AppProviderBasic from './components/layout'
-=======
-import './App.css'
->>>>>>> 95be7c8774205085c40f5fb8bd9a34d07d427fc3
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
     <AppProviderBasic/>
-=======
-
->>>>>>> 95be7c8774205085c40f5fb8bd9a34d07d427fc3
     </>
   )
 }
