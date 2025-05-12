82b9b8e4309df9861f8403fb29d59f3251b4c540
Sebas <sebaslg96@gmail.com>
2025-05-11 23:04:28 -0500

    agregada info en editar

--- frontend/src/pages/medicamentos/edit.jsx
+++ frontend/src/pages/medicamentos/edit.jsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { data, useParams } from 'react-router';
+import { useParams } from 'react-router';
 import { useState, useEffect } from "react";
 import axios from 'axios'
 import Config from '../../Config';
@@ -9,37 +9,42 @@ import Config from '../../Config';
 
 export const MedicamentosEdit = () => {
 
-    const params = useParams()
+  const params = useParams()
 
-    let dataContent = [] 
-    const getData = () =>{
-        axios({
-          method: 'get',
-          url: `${Config('urlRoot')}/medicamento/getbyid/${params.id}`,
-          responseType: 'json'
-        })
-        .then((res) => {
-            const data = res
-            dataContent = data
-        })
-      }
+  const dataPlaceholder = {
+    nombre:null,
+    descripcion: null,
+    existencia:null,
+  }
 
-    getData()
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    axios({
+      method: 'get',
+      url: `${Config('urlRoot')}/medicamento/getbyid/${params.id}`,
+      responseType: 'json'
+    })
+    .then((res) => {
+      const dataSource = res.data.data[0]
+      dataSource ? setData(dataSource) : setData(dataPlaceholder)
+    })
+    .catch(error => console.log(error))
+  },[data])
 
-    console.log(dataContent)
+  //console.log(data)
 
-    return <>
-        <form action="" >
-          <Box sx={{display: 'flex', flexDirection:'column'}}>
-            <TextField margin="dense" required name="nombre" label="Nombre" variant="outlined" />
-            <TextField multiline maxRows={4} margin="dense" required name="descripcion" label="Descripcion" variant="outlined" />
-            <TextField margin="dense" required type="number" name="existencia" label="Existencia" variant="outlined" />
-            <Box>
-              <Button type="submit" variant="contained">Guardar</Button>
-            </Box>
-          </Box>
-        </form>
-    </>;
+  return <>
+    <form action="" >
+      <Box sx={{display: 'flex', flexDirection:'column'}}>
+        <TextField margin="dense" required name="nombre" value={data.nombre} label="Nombre" variant="outlined" />
+        <TextField multiline maxRows={4} margin="dense" value={data.descripcion} required name="descripcion" label="Descripcion" variant="outlined" />
+        <TextField margin="dense" required type="number" value={data.existencia} name="existencia" label="Existencia" variant="outlined" />
+        <Box>
+          <Button type="submit" variant="contained">Guardar</Button>
+        </Box>
+      </Box>
+    </form>
+  </>;
 }
 
 export default MedicamentosEdit
