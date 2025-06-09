c16f238a278704feb52daf6bea8ed334fa6ff921
Sebas <sebaslg96@gmail.com>
2025-06-09 00:56:20 -0500

    creado historia para consultar por paciente

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ import formulaMaestroRoute from './src/routes/formulaMaestro.js'
 import formulaDetalleRoute from './src/routes/formulaDetalle.js'
 import diagnosticosRoute from './src/routes/diagnosticos.js'
 import examenesRoute from './src/routes/examenes.js'
+import historiaRoute from './src/routes/historia.js'
 
 const app = express()
 dotenv.config()
@@ -31,6 +32,7 @@ app.use('/api', formulaMaestroRoute)
 app.use('/api', formulaDetalleRoute)
 app.use('/api', diagnosticosRoute)
 app.use('/api', examenesRoute)
+app.use('/api', historiaRoute)
 
 const port = process.env.PORT || 3000
 app.listen(port,()=>(
