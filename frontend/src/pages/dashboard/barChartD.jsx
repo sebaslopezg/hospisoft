5e3679f39f729590793ec9368f4f2766b685ef29
lulo <lopez.gallego2611@gmail.com>
2025-06-11 08:47:07 -0500

    charts

---
+++ frontend/src/pages/dashboard/barChartD.jsx
@@ -0,0 +1,18 @@
+import { BarChart } from "@mui/x-charts"
+import { cheerfulFiestaPalette } from "@mui/x-charts"
+
+export default function BarChartD(){
+    return <>
+        <BarChart
+        colors={cheerfulFiestaPalette}
+      series={[
+        { data: [35, 44, 24, 34] },
+        { data: [51, 6, 49, 30] },
+        { data: [15, 25, 30, 50] },
+        { data: [60, 50, 15, 25] },
+      ]}
+      height={290}
+      xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'] }]}
+    />
+    </>
+}
---
+++ frontend/src/pages/dashboard/lineChartD.jsx
@@ -0,0 +1,17 @@
+import { LineChart } from "@mui/x-charts"
+import { rainbowSurgePalette } from "@mui/x-charts"
+
+export default function LineChartD(){
+    return <>
+        <LineChart
+        colors={rainbowSurgePalette}
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+                {
+                    data: [2, 5.5, 2, 8.5, 1.5, 5],
+                },
+            ]}
+            height={300}
+/>
+    </>
+}
