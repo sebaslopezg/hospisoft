8bdc3bce271436bae1539921ed7d9277d0c69e24
lulo <lopez.gallego2611@gmail.com>
2025-06-12 13:47:37 -0500

    commit

--- frontend/src/pages/dispensario/index.jsx
+++ frontend/src/pages/dispensario/index.jsx
@@ -2,12 +2,11 @@ import { Outlet } from 'react-router';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import { Card } from '@mui/material';
 
-export default function DispensarioLayout() {
-
+export default function DispensarioLayout(){
   return <>
     <PageContainer>
       <Card>
-      <Outlet />
+      <Outlet/>
       </Card>
     </PageContainer>
   </>
