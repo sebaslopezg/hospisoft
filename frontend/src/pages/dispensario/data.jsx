c03661bb5a20d28c7e9f0dda4385e15d607479a1
Sebas <sebaslg96@gmail.com>
2025-06-12 16:16:56 -0500

    avance en dispensario

--- frontend/src/pages/dispensario/data.jsx
+++ frontend/src/pages/dispensario/data.jsx
@@ -3,37 +3,30 @@ import Config from "../../Config";
 
   const columns = [
   {
-    field: 'numeroFormula',
-    headerName: 'Numero',
-    type: 'text',
-    width: 100,
-  },
-  {
-    field: 'pacienteId',
-    headerName: 'Nombre',
-    type: 'text',
-    width: 150,
-    valueGetter: (row) => {return row.nombre}
-  },
-  {
-    field: 'medico',
-    headerName: 'Medico',
+    field: 'medicamentoId',
+    headerName: 'Medicamento',
     type: 'text',
     width: 250,
     valueGetter: (row) => {return row.nombre}
   },
   {
-    field: 'descripcion',
-    headerName: 'Descripcion',
+    field: 'dosificacion',
+    headerName: 'Dosificacion',
     type: 'text',
-    width: 350,
+    width: 250,
+  },
+  {
+    field: 'cantidad',
+    headerName: 'Cantidad',
+    type: 'text',
+    width: 250,
   },
 ];
 
 const getFormula = (numero) => {
     return axios({
       method: 'get',
-      url: `${Config('urlRoot')}/formula_m/getbynumero/${numero}`,
+      url: `${Config('urlRoot')}/formula_d/getByFormulaNumber/${numero}`,
       responseType: 'json'
     })
 }
