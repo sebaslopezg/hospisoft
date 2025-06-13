fe1e049c3f2235bed1d1947272ace3fca5abb9cf
Sebas <sebaslg96@gmail.com>
2025-06-12 19:12:09 -0500

    agregado navigate para login

--- frontend/src/layouts/dashboard.jsx
+++ frontend/src/layouts/dashboard.jsx
@@ -1,9 +1,17 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { NotificationsProvider } from '@toolpad/core/useNotifications';
 import { DialogsProvider } from '@toolpad/core/useDialogs';
 
 export default function Layout() {
+
+    const navigate = useNavigate()
+
+  const rol = localStorage.getItem('rol')
+  const permiso = (rol == 1 || rol == 2 || rol == 3 || rol == 4)
+
+  permiso ? '' : navigate('/login')
+
   return (
     <NotificationsProvider>
       <DialogsProvider>
