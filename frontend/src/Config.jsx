73ace1b3e50c80bee3f80c4bb6af53e26488a67b
Sebas <sebaslg96@gmail.com>
2025-06-16 22:12:05 -0500

    save

--- frontend/src/Config.jsx
+++ frontend/src/Config.jsx
@@ -3,8 +3,8 @@ const Config = (prop) => {
         //urlRoot:'http://localhost:4000/api'
         //urlRoot:'http://168.231.112.194:4000/api'
         //urlRoot: 'http://192.168.1.120:4000/api'
-        urlRoot: 'http://187.33.157.164:4000/api' // Remoto en clouding
-        //urlRoot: '/api' // For netlify
+        //urlRoot: 'http://187.33.157.164:4000/api' // Remoto en clouding
+        urlRoot: '/api' // For netlify
     }
     return configList[prop];
 }
